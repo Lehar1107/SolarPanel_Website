f0213cfb8a53ba64e2d4001f4a3544f9aa0dd98e
LEhar1107 <agrawallehar66@gmail.comgit config --global user.email agrawallehar66@gmail.com>
2024-12-02 21:51:19 +0530

    completed testimonials

--- script.js
+++ script.js
@@ -6,3 +6,39 @@ menuOpenButton.addEventListener("click" , () => {
 });
 
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Import Swiper
+//import Swiper from 'swiper';
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+    // Optional parameters
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+  });
